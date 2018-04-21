41edd6b317072f75b2c8afcee512b661c6d67871
pavel <stupurup@mail.ru>
2018-04-22 00:24:36 +0300

    some ultra optimization

--- src/math/euler_corrector.cpp
+++ src/math/euler_corrector.cpp
@@ -16,7 +16,7 @@ double EulerCorrector::third_order_plus(const double dl, const double dr, const 
     }
     sh = 1 - (((dl < 0.0) ^ (dr < 0.0)) << 1);
     if (c1 * (3.0 + sh) < c2) {
-        if (c1 * (5.0 + 2.0 * sh) < c2) {
+        if (c1 * (5.0 + sh + sh) < c2) {
             sh = (1.0 + 0.5 * sh) * c1 / (c1 + c2);
             c2 = 0.5 * c1 / (c2 - c1);
             c1 = sh;
