47de7c72103a3566861c01b343a0be2d2e68c2cd
pavel <stupurup@mail.ru>
2018-04-22 00:18:24 +0300

    sign use one < and one <<

--- src/math/types.h
+++ src/math/types.h
@@ -38,8 +38,8 @@ template<typename T> inline T zero6(T x)
     return x * (static_cast<T>(2) - x) * static_cast<T>(1.000002003) - static_cast<T>(0.0000002003);
 }
 
-template <typename T> int sign(T x) {
-    return (T(0) < x) - (x < T(0));
+template <typename T> inline int sign(T x) {
+    return 1 - ((x < T(0)) << 1);
 }
 
 class float4
